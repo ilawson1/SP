2286ad9af8c501b9504ced2d3abd5ccf5fc03e10
ilawson1 <ilawson1@binghamton.edu>
2017-11-24 14:27:52 -0500

    Update DFT_Real_Guitar.m
    Fixes for dB plots and corresponding y limits

--- DFT_Real_Guitar.m
+++ DFT_Real_Guitar.m
@@ -1,5 +1,6 @@
 % Sam Stone and Isaac Lawson-Hughes
 function DFT_Real_Guitar()
+close all
 Fs = 4410;
 T = 1/Fs;
 Nzp = 2^15;
@@ -49,29 +50,34 @@ t = ((-Nzp/2):(Nzp/2-1))*Fs/Nzp; % used to plot DFT
 
 % A
 figure;
-subplot(2,1,1); plot(t,log(abs(X_A))/log(10));
+subplot(2,1,1); plot(t,20*log10(abs(X_A)));
 title('|X_A| in db vs f');
-axis([0 1000 -3 3]);
-subplot(2,1,2); plot(t,log(abs(X_A_H))/log(10));
+xlim([0 1000]);
+ylim auto;
+subplot(2,1,2); plot(t,20*log10(abs(X_A_H)));
 title('|X_A| with hamming window in db vs f');
-axis([0 1000 -3 3]);
+xlim([0 1000]);
+ylim auto;
 
 % D
 figure;
-subplot(2,1,1); plot(t,log(abs(X_D))/log(10));
+subplot(2,1,1); plot(t,20*log10(abs(X_D)));
 title('|X_D| in db vs f');
-axis([0 1000 -3 3]);
-subplot(2,1,2); plot(t,log(abs(X_D_H))/log(10));
+xlim([0 1000]);
+ylim auto;
+subplot(2,1,2); plot(t,20*log10(abs(X_D_H)));
 title('|X_D| with hamming window in db vs f');
-axis([0 1000 -3 3]);
+xlim([0 1000]);
+ylim auto;
 
 % E
 figure;
-subplot(2,1,1); plot(t,log(abs(X_E))/log(10));
+subplot(2,1,1); plot(t,20*log10(abs(X_E)));
 title('|X_E| in db vs f');
-axis([0 1000 -3 3]);
-subplot(2,1,2); plot(t,log(abs(X_E_H))/log(10));
+xlim([0 1000]);
+ylim auto;
+subplot(2,1,2); plot(t,20*log10(abs(X_E_H)));
 title('|X_E| with hamming window in db vs f');
-axis([0 1000 -3 3]);
-
+xlim([0 1000]);
+ylim auto;
 
